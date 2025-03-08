d1065012f1c73783d529bfc435621fec401ffff1
Aoi3u <miu.blue3@gmail.com>
2025-03-09 01:58:33 +0900

    feeature: タスク削除機能

--- src/main.ts
+++ src/main.ts
@@ -18,10 +18,18 @@ const onClickAdd = () => {
   // button(完了)タグ生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+
+  });
 
   // button(削除)タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親にあるliタグを未完了リストから削除
+    const deleteTarget = deleteButton.closest("li");
+    document.getElementById("incomplete-list")?.removeChild(deleteTarget);
+  });
 
 
   // liタグの子要素に各要素を設定
